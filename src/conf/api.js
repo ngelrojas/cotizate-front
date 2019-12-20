0d8681647506c9bd201a7cc514206af2a1550505
ngel rojas p <aomarrojasp@gmail.com>
2019-12-20 19:08:09 -0300

    update configuration server

--- src/conf/api.js
+++ src/conf/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from 'axios' 
 
-const URL_DEV = `http://127.0.0.1:8000/api/v1`
-const URL_PROD = `http://www.cotizate.com:8000/api/v1`
+// const URL_PROD = `http://127.0.0.1:8000/api/v1`
+const URL_PROD = `http://54.225.32.34:8000/api/v1`
 
 export default axios.create({
-    baseURL: URL_DEV 
+    baseURL: URL_PROD 
 })
