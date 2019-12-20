fe9f34499b59c1db9476a9987ebba964133534a2
ngel rojas p <aomarrojasp@gmail.com>
2019-12-20 19:05:00 -0300

    update register user

---
+++ build/precache-manifest.5e06096fc4db2a2482bd7d68f736d3e4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6980f5bbf98518b1d1b7196932037a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69bfd9786c49ffb71262",
+    "url": "/static/css/2.398032ed.chunk.css"
+  },
+  {
+    "revision": "604e24f9254610c793d1",
+    "url": "/static/css/main.15b92892.chunk.css"
+  },
+  {
+    "revision": "69bfd9786c49ffb71262",
+    "url": "/static/js/2.4a66899b.chunk.js"
+  },
+  {
+    "revision": "604e24f9254610c793d1",
+    "url": "/static/js/main.309bf565.chunk.js"
+  },
+  {
+    "revision": "a3b41f518f202334f6f6",
+    "url": "/static/js/runtime-main.841bf76c.js"
+  },
+  {
+    "revision": "3b70e2c3f8f906c90248e58dba368bc2",
+    "url": "/static/media/background-one.3b70e2c3.jpg"
+  },
+  {
+    "revision": "14b18e86e1f897bd87903efeb380366d",
+    "url": "/static/media/cancel.14b18e86.svg"
+  },
+  {
+    "revision": "9a1e152c62cc966d51db275e6b6950f4",
+    "url": "/static/media/cotizate-tech.9a1e152c.png"
+  },
+  {
+    "revision": "20caac44e56a1dea6bff143991397f98",
+    "url": "/static/media/facebook.20caac44.svg"
+  },
+  {
+    "revision": "77c4fc22b5f2b94e1537252cccd0607b",
+    "url": "/static/media/gps.77c4fc22.svg"
+  },
+  {
+    "revision": "9d53d97bb11e5a9b3b6014b0f1d6e46e",
+    "url": "/static/media/heart.9d53d97b.svg"
+  },
+  {
+    "revision": "8eb327b7dc76268b9aef8942b78b8eca",
+    "url": "/static/media/instagram.8eb327b7.svg"
+  },
+  {
+    "revision": "c207f59693a15a036da0efd77124cd2d",
+    "url": "/static/media/left-arrow.c207f596.svg"
+  },
+  {
+    "revision": "db444cb6971485dbedc40b3ffcb14acb",
+    "url": "/static/media/linkedin.db444cb6.svg"
+  },
+  {
+    "revision": "f1a9d1d8d6c489183940ae98992d7b73",
+    "url": "/static/media/mastercard.f1a9d1d8.svg"
+  },
+  {
+    "revision": "1ee4c3ee10a89d4ebe3782b2ff0d9eea",
+    "url": "/static/media/right-arrow.1ee4c3ee.svg"
+  },
+  {
+    "revision": "a29e3021b021ad1cc5ac9273df5516c2",
+    "url": "/static/media/rounded.a29e3021.jpg"
+  },
+  {
+    "revision": "7b3eeb225556db707011f0f59ab2ef08",
+    "url": "/static/media/search.7b3eeb22.svg"
+  },
+  {
+    "revision": "59c8c84e7ba4ea1a53d3d03500a5321c",
+    "url": "/static/media/twitter.59c8c84e.svg"
+  },
+  {
+    "revision": "c6b1796218d6d1f0e15be1fa549e5d89",
+    "url": "/static/media/visa.c6b17962.svg"
+  }
+]);
