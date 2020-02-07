ff5c02b78e027ee66d01483b6b8ada33c21d6ca6
ngel rojas p <aomarrojasp@gmail.com>
2020-02-07 00:36:45 -0300

    add new editor

--- src/pages/create_projects/components/create_form/history.js
+++ src/pages/create_projects/components/create_form/history.js
@@ -1,159 +1,160 @@
-import React from 'react';
-import { Editor } from 'react-draft-wysiwyg';
+import React from 'react'
+// import {Editor} from 'react-draft-wysiwyg'
+import {Editor} from '@tinymce/tinymce-react'
 
+class HistoryForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      fields: {},
+      error: {},
+      excerpt: '',
+      descripcion: '',
+    }
+  }
 
-function  uploadImageCallBack(file) {
-        return new Promise(
-          (resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', 'https://api.imgur.com/3/image');
-            xhr.setRequestHeader('Authorization', 'Client-ID 8d26ccd12712fca');
-            const data = new FormData();
-              data.append('image', file);
-            xhr.send(data);
-            xhr.addEventListener('load', () => {
-              const response = JSON.parse(xhr.responseText);
-              resolve(response);
-            });
-            xhr.addEventListener('error', () => {
-              const error = JSON.parse(xhr.responseText);
-              reject(error);
-            });
-          }
-        );
+  handleEditorChange = (content, editor) => {
+    console.log('Content was updated:', content)
+  }
+
+  handleChange = e => {
+    e.preventDefault()
+    let fields = this.state.fields
+    fields[e.target.name] = e.target.value
+    this.setState({fields})
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    let excerpt_data = this.state.excerpt
+    let description_data = this.state.descripcion
+    let basic_data = JSON.parse(window.localStorage.getItem('basic'))
+    //console.log(data[0]);
+    console.log(excerpt_data)
+    console.log(description_data)
+    if (this.validateForm()) {
+    }
+  }
+
+  validateForm() {
+    let fields = this.state.fields
+    let errors = {}
+    let formIsValid = true
+    let msg = {}
+
+    if (!fields['video']) {
+      formIsValid = false
+      errors['video'] = 'Tu proyecto debe contener video.'
+    }
+  }
+
+  render() {
+    return (
+      <div className="container-site_on">
+        <form method="post" onSubmit={this.handleSubmit}>
+          <div className="container form-step-one">
+            <div className="form-group">
+              <label className="col-md-8">
+                <p>
+                  <span className="form-sub-title">
+                    Agregar un video o imagen para la parte superior de tu
+                    proyecto.
+                  </span>
+                </p>
+                <p>
+                  <span className="form-sub-title">
+                    Los proyectos con videos aumentan un porcentaje mas que los
+                    proyectos sin video, Has un video de 2-3 minutos.
+                  </span>
+                </p>
+              </label>
+              <label className="col-md-8">
+                <span className="form-sub-title">Coloca un video</span>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={this.state.fields.video || ''}
+                  name="video"
+                  onChange={this.handleChange}
+                />
+              </label>
+            </div>
+            <div className="form-group">
+              <label className="col-md-8">
+                <span className="form-sub-title">
+                  Cuenta un poco sobre tu proyecto (resumen)
+                </span>
+                <Editor
+                  initialValue="<p>This is the initial content of the editor</p>"
+                  init={{
+                    height: 500,
+                    menubar: true,
+
+                    plugins: [
+                      'advlist autolink lists link image charmap print preview anchor image',
+                      'imagetools earchreplace visualblocks code fullscreen',
+                      'insertdatetime media table paste code help wordcount',
+                    ],
+                    toolbar:
+                      'undo redo | formatselect | bold italic backcolor | \
+                     alignleft aligncenter alignright alignjustify | image | \
+                     imagetools bullist numlist outdent indent | removeformat | help',
+                    automatic_uploads: true,
+                    file_picker_types: 'image',
+                    file_picker_callback: function(cb, value, meta) {
+                      var input = document.createElement('input')
+                      input.setAttribute('type', 'file')
+                      input.setAttribute('accept', 'image/*')
+                      input.onchange = function() {
+                        var file = this.files[0]
+
+                        var reader = new FileReader()
+                        reader.onload = function() {
+                          /*
+          Note: Now we need to register the blob in TinyMCEs image blob
+          registry. In the next release this part hopefully won't be
+          necessary, as we are looking to handle it internally.
+        */
+                          var id = 'blobid' + new Date().getTime()
+                          var blobCache =
+                            window.tinymce.activeEditor.editorUpload.blobCache
+                          var base64 = reader.result.split(',')[1]
+                          var blobInfo = blobCache.create(id, file, base64)
+                          blobCache.add(blobInfo)
+
+                          /* call the callback and populate the Title field with the file name */
+                          cb(blobInfo.blobUri(), {title: file.name})
+                        }
+                        reader.readAsDataURL(file)
+                      }
+
+                      input.click()
+                    },
+                  }}
+                  onEditorChange={this.handleEditorChange}
+                />
+              </label>
+            </div>
+            <div className="form-group">
+              <label className="col-md-8">
+                <span className="form-sub-title">
+                  Cuenta por que tu proyecto es tan relevante, has una
+                  descripcion completa de tu proyecto.
+                </span>
+              </label>
+            </div>
+          </div>
+          <div className="container">
+            <div className="col col-md-8">
+              <button type="submit" className="btn btn-primary">
+                Guardar
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
-class HistoryForm extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            fields: {},
-            error: {},
-            excerpt: '',
-            descripcion: '',
-        }
-    }
-
-    onContentStateChange(contentState) {
-         
-        // console.log(contentState);
-    };
-    
-    onContentStateChange_ = (contentState) => {
-        this.setState({descripcion: contentState}); 
-        // console.log(contentState);
-    };
-    
-    _onContentStateChange_ = (contentState) => {
-        
-        // this.setState({excerpt: contentState}); 
-        //return contentState.blocks[0].text
-    };
-
-    handleChange = e =>{
-        e.preventDefault();
-        let fields = this.state.fields;
-        fields[e.target.name] = e.target.value;
-        this.setState({fields})
-    }
-
-    handleSubmit = e => {
-        e.preventDefault();
-        let excerpt_data = this.state.excerpt;
-        let description_data = this.state.descripcion;
-        let basic_data = JSON.parse(window.localStorage.getItem("basic"));
-        //console.log(data[0]);
-        console.log(excerpt_data);
-        console.log(description_data)
-        if(this.validateForm()){
-            
-
-            
-        }
-        
-    }
-
-    validateForm(){
-        let fields = this.state.fields;
-        let errors = {};
-        let formIsValid = true;
-        let msg = {}
-
-        if(!fields['video']){
-            formIsValid = false;
-            errors['video'] = 'Tu proyecto debe contener video.';
-        }
-    }
-
-    render(){
-        return(
-     <div className="container-site_on">
-                <form method="post" onSubmit={this.handleSubmit}> 
-                    <div  className="container form-step-one">
-                        <div className="form-group">
-                            <label className="col-md-8">
-                                <p><span className="form-sub-title">Agregar un video o imagen para la parte superior de tu proyecto.</span></p>
-                                <p><span className="form-sub-title">Los proyectos con videos aumentan un porcentaje mas que los proyectos sin video, Has un video de 2-3 minutos.</span></p>
-                                
-                            </label>
-                            <label className="col-md-8"><span className="form-sub-title">Coloca un video</span>
-                            <input 
-                                type="text" 
-                                className="form-control"
-                                value={this.state.fields.video || ''}
-                                name="video"
-                                onChange={this.handleChange}
-                            />
-                            </label>
-                        </div>
-                        <div className="form-group">
-                            <label className="col-md-8"><span className="form-sub-title">Cuenta un poco sobre tu proyecto (resumen)</span>
-                            <Editor
-                                wrapperClassName="demo-wrapper"
-                                editorClassName="demo-editor"
-                                onContentStateChange={this._onContentStateChange_}
-                                toolbar={{
-                                    inline: { inDropdown: true },
-                                    list: { inDropdown: true },
-                                    textAlign: { inDropdown: true },
-                                    link: { inDropdown: true },
-                                    history: { inDropdown: true },
-                                    image: { uploadCallback: uploadImageCallBack, 
-                                        alt: { present: true, mandatory: false },
-                                        previewImage: true},
-                                }}
-                            />
-                            </label>
-                        </div>
-                        <div className="form-group">
-                            <label className="col-md-8"><span className="form-sub-title">Cuenta por que tu proyecto es tan relevante, has una descripcion completa de tu proyecto.</span>
-                            <Editor
-                                wrapperClassName="demo-wrapper"
-                                editorClassName="demo-editor"
-                                onContentStateChange={this.onContentStateChange_}
-                                toolbar={{
-                                    inline: { inDropdown: true },
-                                    list: { inDropdown: true },
-                                    textAlign: { inDropdown: true },
-                                    link: { inDropdown: true },
-                                    history: { inDropdown: true },
-                                    image: { uploadCallback: uploadImageCallBack, 
-                                        alt: { present: true, mandatory: false } },
-                                }}
-                            />
-                            </label>
-                        </div>
-                        
-                    </div>
-                    <div className="container">
-                        <div className="col col-md-8">
-                            <button type="submit" className="btn btn-primary">Guardar</button>
-                        </div>
-                    </div>
-                </form>
-            </div>        
-        ) 
-    }
-}
-
-export default HistoryForm;
+export default HistoryForm
